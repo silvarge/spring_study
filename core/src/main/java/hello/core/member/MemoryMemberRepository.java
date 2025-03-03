b497b40058c0f2c2fb5229c3258b6ba2240d94f3
silvarge <sep.non2@gmail.com>
2025-03-04 00:19:55 +0900

    [Feat] 회원 도메인 개발 - 회원 엔티티 - 회원 저장소 (인터페이스/구현체) - 회원 서비스 (인터페이스/구현체)
    ## 문제점
    - 의존 관계가 인터페이스 뿐만 아니라 구현 관계까지 모두 의존

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    // HashMap - 동시성 이슈가 발생할 수 있음
+    // ConcurrentHashMap - 을 현업에서는 사용하는 편
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
