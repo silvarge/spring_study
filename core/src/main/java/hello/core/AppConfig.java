9b5c0658c00313cb0b1020effa3e15dec9502b71
silvarge <sep.non2@gmail.com>
2025-05-16 11:54:23 +0900

    Session 6: 싱글톤 컨테이너
    - 싱글톤 패턴
    - 싱글톤 컨테이너
    - 싱글톤 방식의 주의점
    - @Configuration과 싱글톤
      - @Bean만 있어도 스프링 빈 등록이 가능하다
      - 싱글톤을 보장해 주는 것은 @Configuration

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,19 +15,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+  // @Bean memberService -> new MemoryMemberRepository()
+  // @Bean orderService -> new MemoryMemberRepository()
+  // 2개 모두 생성한다면 싱글톤이 깨지는 거 아닌가? -> @Configuration 이 싱글톤을 보장해 줌
+
   // 역할이 잘 드러난다
   @Bean
   public MemberService memberService() {
+    System.out.println("AppConfig.memberService");
     return new MemberServiceImpl(memberRepository());
   }
 
   @Bean
   public MemberRepository memberRepository() {
+    System.out.println("AppConfig.memberRepository");
     return new MemoryMemberRepository();
   }
 
   @Bean
   public OrderService orderService(){
+    System.out.println("AppConfig.orderService");
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
---
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,41 @@
+package hello.core.singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class StatefulServiceTest {
+
+  @Test
+  @DisplayName("")
+  void statefulServiceSingleton() {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+    StatefulService statefulService1 = ac.getBean(StatefulService.class);
+    StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+    // ThreadA: A사용자가 10000원 주문
+    int price = statefulService1.order("userA", 10000);
+    // ThreadB: B사용자가 20000원 주문
+    statefulService2.order("userB", 20000);
+
+    // ThreadA: 사용자A 주문 금액 조회
+//    int price = statefulService1.getPrice();
+    System.out.println("price = " + price);
+
+    assertThat(price).isEqualTo(10000);
+  }
+
+  static class TestConfig {
+    @Bean
+    public StatefulService statefulService() {
+      return new StatefulService();
+    }
+  }
+
+}
